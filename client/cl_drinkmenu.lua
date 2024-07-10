c406408b9940b66d1d0c8abd949a4cc5cb7377f9
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-07-09 23:09:11 -0700

    Support for ox_target

--- client/cl_drinkmenu.lua
+++ client/cl_drinkmenu.lua
@@ -1,24 +1,48 @@
 local QBCore = exports['qb-core']:GetCoreObject()
------ | CREATING DRINK MENU LOCATION | -----
-exports['qb-target']:AddBoxZone("TacoJobDrinkMenu", vector3(13.32, -1595.9, 29.38), 0.9, 0.9, {
-	name = "TacoJobDrinkMenu",
-	heading = 347.27,
-	debugPoly = false,
-	minZ = 29.38 - 2,
-	maxZ = 29.38 + 2,
-}, {
-	options = {
-		{
-            type = "client",
-            event = "bd-tacojob:client:OpenDrinkMenu",
-			icon = "fa-solid fa-faucet-drip",
-			label = "Drink Menu",
-			job = "taco",
-		},
-	},
-	distance = 2.5
-})
------ | CREATING MENU | -----
+local ox_inventory = exports.ox_inventory
+
+if  Config.TargetSystem == 'ox' then
+  ----- | CREATING DRINK MENU LOCATION | -----
+  exports.ox_target:addBoxZone({
+		coords = vec4(13.32, -1595.9, 29.38, 222.49),
+		size = vec3(2, 2, 2),
+		rotation = 45,
+		debug = drawZones,
+		options = {
+			{
+				name = 'tacojobdrinkmenu',
+				event = 'bd-tacojob:client:OpenDrinkMenu',
+				icon = 'fa-solid fa-faucet-drip',
+				label = 'Drinks Menu',
+				groups = {
+					taco = 0
+				},
+			},
+		}
+	})
+elseif Config.TargetSystem == 'qb' then
+  ----- | CREATING DRINK MENU LOCATION | -----
+  exports['qb-target']:AddBoxZone("TacoJobDrinkMenu", vector3(13.32, -1595.9, 29.38), 0.9, 0.9, {
+    name = "TacoJobDrinkMenu",
+    heading = 347.27,
+    debugPoly = false,
+    minZ = 29.38 - 2,
+    maxZ = 29.38 + 2,
+  }, {
+    options = {
+      {
+              type = "client",
+              event = "bd-tacojob:client:OpenDrinkMenu",
+        icon = "fa-solid fa-faucet-drip",
+        label = "Drink Menu",
+        job = "taco",
+      },
+    },
+    distance = 2.5
+  })
+end
+
+----- | CREATING DRINK MENU | -----
 lib.registerContext({
     id = 'taco_drink_menu',
     title = 'Drink Menu',
