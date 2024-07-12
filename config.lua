d20829e1bb248e876f6e1e017529258e2be810ca
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-07-11 18:58:57 -0700

    Default = qb

--- config.lua
+++ config.lua
@@ -12,7 +12,7 @@
 --]]
 Config = {}
 
-Config.TargetSystem = 'ox' -- Supports 'qb' & 'ox'
+Config.TargetSystem = 'qb' -- Supports 'qb' & 'ox'
 Config.InventorySystem = 'qb' -- Supports 'qb' | Looking for tester for ox. contact me on discord @ baylorai
 
 ----- | PED STUFF | -----
