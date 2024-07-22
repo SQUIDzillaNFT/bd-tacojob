f452cb00145e94f607150f57591015a6b024f3bc
BayloraiEX <117157428+BayloraiEX@users.noreply.github.com>
2024-07-21 22:12:25 -0700

    Update 0.1.0

--- server/sv_drinkmenu.lua
+++ server/sv_drinkmenu.lua
@@ -1,7 +1,130 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+local ox_inventory = exports.ox_inventory
 
------ | MAKE Orang O Tang | -----
-RegisterNetEvent('bd-tacojob:server:makeOrangTang', function()
+if Config.InventorySystem == 'ox' then
+  -- ORANG TANG --
+  RegisterNetEvent('bd-tacojob:server:makeOrangTang', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+    if not ox_inventory:RemoveItem(src, 'th_oranges', 2, false) then
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You need 2x Oranges to make a Orang-O-Tang',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      }) return end
+      ox_inventory:AddItem(src, 'th_orangotang', 2)
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You have poured 2x Orang-O-Tang',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      })
+  end)
+  -- SPRUNK --
+  RegisterNetEvent('bd-tacojob:server:makeSprunk', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+    if not ox_inventory:RemoveItem(src, 'th_carbonated_water', 2, false) then
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You need 2x Carbonated Water to make Sprunk',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      }) return end
+      ox_inventory:AddItem(src, 'th_sprunk', 2)
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You have poured 2x Sprunk',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      })
+  end)
+  -- ECOLA --
+  RegisterNetEvent('bd-tacojob:server:makeEcola', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+    if not ox_inventory:RemoveItem(src, 'th_carbonated_water', 2, false) then
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You need 2x Carbonated Water to make Ecola',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      }) return end
+      ox_inventory:AddItem(src, 'th_ecola', 2)
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You have poured 2x Ecola',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      })
+  end)
+elseif Config.InventorySystem == 'qb' then
+  ----- | MAKE Orang O Tang | -----
+  RegisterNetEvent('bd-tacojob:server:makeOrangTang', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
@@ -40,53 +163,9 @@ RegisterNetEvent('bd-tacojob:server:makeOrangTang', function()
       icon = 't',
       iconColor = '#FA8072'
     })
-end)
-
-/* SPONSORED BY SPRUNK SO UNCOMMENT THIS IF YOU WANT TO ADD ECOLA
------ | MAKE ECOLA | -----
-RegisterNetEvent('bd-tacojob:server:makeEcola', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if not Player then return end
-    if not exports['qb-inventory']:RemoveItem(src, 'th_carbonated_water', 2, false) then
-      lib.notify(source, {
-        id = 'taco_job',
-        title = 'Taco Hut',
-        description = 'You need Carbonated Water to make a Ecola',
-        showDuration = false,
-        position = 'top-right',
-        style = {
-          backgroundColor = '#141517',
-          color = '#FA8072',
-          ['.description'] = {
-              color = '#909296'
-          }
-      },
-        icon = 't',
-        iconColor = '#FA8072'
-      }) return end
-    exports['qb-inventory']:AddItem(src, 'th_ecola', 2, false, false)
-    TriggerClientEvent('qb-inventory:client:ItemBox', source, QBCore.Shared.Items['th_ecola'], 'add', 2)
-    lib.notify(source, {
-      id = 'taco_job',
-      title = 'Taco Hut',
-      description = 'You have poured a Ecola',
-      showDuration = false,
-      position = 'top-right',
-      style = {
-        backgroundColor = '#141517',
-        color = '#FA8072',
-        ['.description'] = {
-            color = '#909296'
-        }
-    },
-      icon = 't',
-      iconColor = '#FA8072'
-    })
-end)
-*/
------ | MAKE SPRUNK | -----
-RegisterNetEvent('bd-tacojob:server:makeSprunk', function()
+  end)
+  ----- | MAKE SPRUNK | -----
+  RegisterNetEvent('bd-tacojob:server:makeSprunk', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
@@ -125,4 +204,46 @@ RegisterNetEvent('bd-tacojob:server:makeSprunk', function()
       icon = 't',
       iconColor = '#FA8072'
     })
-end)
+  end)
+  ----- | MAKE ECOLA | -----
+  RegisterNetEvent('bd-tacojob:server:makeEcola', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    if not Player then return end
+    if not exports['qb-inventory']:RemoveItem(src, 'th_carbonated_water', 2, false) then
+      lib.notify(source, {
+        id = 'taco_job',
+        title = 'Taco Hut',
+        description = 'You need Carbonated Water to make a Ecola',
+        showDuration = false,
+        position = 'top-right',
+        style = {
+          backgroundColor = '#141517',
+          color = '#FA8072',
+          ['.description'] = {
+              color = '#909296'
+          }
+      },
+        icon = 't',
+        iconColor = '#FA8072'
+      }) return end
+    exports['qb-inventory']:AddItem(src, 'th_ecola', 2, false, false)
+    TriggerClientEvent('qb-inventory:client:ItemBox', source, QBCore.Shared.Items['th_ecola'], 'add', 2)
+    lib.notify(source, {
+      id = 'taco_job',
+      title = 'Taco Hut',
+      description = 'You have poured a Ecola',
+      showDuration = false,
+      position = 'top-right',
+      style = {
+        backgroundColor = '#141517',
+        color = '#FA8072',
+        ['.description'] = {
+            color = '#909296'
+        }
+    },
+      icon = 't',
+      iconColor = '#FA8072'
+    })
+  end)
+end
